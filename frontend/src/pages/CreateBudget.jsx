988c1ecad857d03385fc663203329711144fe439
DeepanshuYadavd <dipanshuy591@gmail.com>
2026-09-02 12:07:59 +0530

    create budget form

--- frontend/src/pages/CreateBudget.jsx
+++ frontend/src/pages/CreateBudget.jsx
@@ -3,6 +3,10 @@ import apiClient from "../ApiClient/interceptor";
 const CreateBudget = () => {
   const [categories, setCategories] = useState([]);
   const [categoryLoader, setcateoryLoader] = useState(true);
+  const [createCategory, setCreateCategory] = useState(false);
+  const [createCategoryData, setcreateCategoryData] = useState({
+    category: "",
+  });
   const getCategories = async () => {
     try {
       const response = await apiClient.get("/category/get");
@@ -16,7 +20,79 @@ const CreateBudget = () => {
   useEffect(() => {
     getCategories();
   }, []);
-  console.log(categories, "testttcat");
+
+  const handleCategoryChange = (e) => {
+    setcreateCategoryData({
+      ...createCategoryData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleCategorySubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await apiClient.post("/category/create", createCategoryData);
+      getCategories();
+      setcreateCategoryData({
+        category: "",
+      });
+      setCreateCategory(!createCategory);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const month = [
+    {
+      value: 1,
+      name: "January",
+    },
+    {
+      value: 2,
+      name: "February",
+    },
+    {
+      value: 3,
+      name: "March",
+    },
+    {
+      value: 4,
+      name: "April",
+    },
+    {
+      value: 5,
+      name: "May",
+    },
+    {
+      value: 6,
+      name: "June",
+    },
+    {
+      value: 7,
+      name: "July",
+    },
+    {
+      value: 8,
+      name: "August",
+    },
+    {
+      value: 9,
+      name: "September",
+    },
+    {
+      value: 10,
+      name: "October",
+    },
+    {
+      value: 11,
+      name: "November",
+    },
+    {
+      value: 12,
+      name: "December",
+    },
+  ];
+
   return (
     <>
       <main>
@@ -51,6 +127,49 @@ const CreateBudget = () => {
                 </select>
               </>
             )}
+
+            <div className="create-category">
+              {createCategory ? (
+                <>
+                  <input
+                    type="text"
+                    placeholder="Enter Category"
+                    onChange={handleCategoryChange}
+                    name="category"
+                    value={createCategoryData.category}
+                  />{" "}
+                  <button onClick={handleCategorySubmit}>save</button>{" "}
+                  <button onClick={() => setCreateCategory(!createCategory)}>
+                    cancel
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => setCreateCategory(!createCategory)}>
+                    create category
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+
+          <div className="amount">
+            <label>Amount:</label>
+            <input type="number" placeholder="Enter Your Amount" />
+          </div>
+          <div className="month">
+            <label>Month:</label>
+            <select>
+              {month.map((m) => (
+                <option key={m.value} value={m.value}>
+                  {m.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="year">
+            <label>Year:</label>
+            <input type="number" placeholder="Enter Your Year" />
           </div>
         </form>
       </main>
